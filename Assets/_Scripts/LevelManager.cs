55dd096c987b054f5664aa1069b205da00fc1847
Ali Kedwaii <ali.kedwaii@utexas.edu>
2019-01-26 13:07:37 -0600

    transition coroutines

---
+++ Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    #region singleton
+
+    public static LevelManager instance { get; private set; }
+
+    private float fadeTime = 1.0f;
+
+    private void Awake(){
+        if (instance != null){
+            Destroy(gameObject);
+        }
+        else {
+            instance = this;
+        }
+        DontDestroyonLoad(gameObject);
+    }
+
+    #endregion
+
+    private IEnumerator SceneFade(String scenename){
+        String _scenename = scenename;
+        //DisableControls
+        yield return null;
+        
+        //BlackScreen = GetComponent<>();
+        for (float timePassed = 0; timePassed < fadeTime; timePassed += Time.deltaTime){
+            //BlackScreen.alpha = Mathf.Lerp(1.0f, 0.0f, timePassed);
+            yield return null;
+        }
+        SceneManager.LoadScene(_scenename, LoadSceneMode.Single);
+        yield return null;
+
+        //BlackScreen = GetComponent<>();
+        for (float timePassed = 0; timePassed < fadeTime; timePassed += Time.deltaTime){
+            //BlackScreen.alpha = Mathf.Lerp(0.0f, 1.0f, timePassed);
+            yield return null;
+        }
+        
+        //EnableControls
+    }
+}
--- Assets/_Scripts/MusicManager.cs
+++ Assets/_Scripts/MusicManager.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    
+    #region singleton
+    public static MusicManager instance { get; private set; }
+    public AudioSource MainSource;
+    public AudioSource LayerSource;
+    private bool paused;
+
+    private float fullVol = 1.0f;
+    private float noVol = 0.0f;
+    private float fadeTime = 1.0f;
+    private float mainMixVol = 0.7f;
+    private float layerMixVol = 0.4f;
+
+    private void Awake(){
+     MainSource = GetComponent<AudioSource>();
+        paused = false;
+        if (instance != null){
+            Destroy(gameObject);
+        }
+        else {
+            instance = this;
+        }
+        DontDestroyonLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    #endregion
+
+    public void Play(String name){
+     MainSource.clip = Resources.Load<AudioClip>(name);
+     MainSource.Play();
+    }
+
+    public void Stop(){
+     MainSource.Stop();
+    }
+    
+    // public void onMenu(){
+    //     paused = !paused;
+    //     if (paused){
+    //         MainSource.volume /= 2;
+    //     } else {
+    //         MainSource.volume *= 2;
+    //     }
+    // }
+
+    private IEnumerator MusicSwap(String songname){
+        String _songname = songname;
+        yield return null;
+
+        for (float timePassed = 0; timePassed < fadeTime; timePassed += Time.deltaTime){
+            float vol = Mathf.Lerp(fullVol, noVol, timePassed);
+         MainSource.volume = vol;
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+     MainSource.Stop();
+     MainSource.clip = Resources.Load<AudioClip>(_songname);
+     MainSource.Play();
+
+        for (float timePassed = 0; timePassed < fadeTime; timePassed += Time.deltaTime){
+            float vol = Mathf.Lerp(noVol, fullVol, timePassed);
+         MainSource.volume = vol;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator MusicLayer(String songname){
+        String _songname = songname;
+        yield return null;
+
+        for (float timePassed = 0; timePassed < fadeTime; timePassed += Time.deltaTime){
+            float mainVol = Mathf.Lerp(fullVol, mainMixVol, timePassed);
+            float layerVol = Mathf.Lerp(noVol, layerMixVol, timePassed);
+            //Alternatively, fade out main and fade in layer 100% for crossfade
+            MainSource.volume = mainVol;
+            LayerSource.volume = layerVol;
+
+            yield return null;
+        }
     }
 }
