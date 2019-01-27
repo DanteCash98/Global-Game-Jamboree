804d987acc08bc8bcbb8bea9ff3c0db9aac803d4
Dante Cash <DanteCash98@gmail.com>
2019-01-27 12:21:35 -0600

    mend

---
+++ Assets/_Scripts/CroissantAI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CroissantAI : MonoBehaviour, ITakeDamage
+{
+    public float speed = 2f;
+    public GameObject player;
+    public float health = 30f;
+
+    private float startYPos;
+
+    public void Crunch()
+    {
+        startYPos = transform.position.y;
+        StartCoroutine(move());
+    }
+
+    IEnumerator move()
+    {
+    
+        Vector3 targetPos = (Random.value > .5f) ? 
+            Vector3.right.WithValues(y: transform.position.y): 
+            Vector3.left.WithValues(y: transform.position.y);
+        
+        while (transform.position != targetPos)
+        {
+
+            Mathf.Clamp(transform.position.y, startYPos,startYPos);
+            
+            transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * speed);
+            yield return null;
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.gameObject.SendMessage("AddLight", -0.1f,SendMessageOptions.RequireReceiver);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        Debug.Log("Croissant: Damage Taken");
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
