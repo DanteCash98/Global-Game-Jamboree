9607c3c60f617d5fcb58b0391bc1315ea8ba918a
Clayton Stamper <claystamper@gmail.com>
2019-01-26 20:50:59 -0600

    combat sandbox working

--- Assets/_Scripts/DarknessSut.cs
+++ Assets/_Scripts/DarknessSut.cs
@@ -5,6 +5,31 @@ using UnityEngine;
 public class DarknessSut : MonoBehaviour, ITakeDamage {
     
     public GameObject explosionParticle;
+    private Transform player;
+
+    [SerializeField] private float lerpSpeed = 1f;
+    [SerializeField] private Vector3 offset;
+
+    private void Start() {
+        player = Player.instance.transform;
+    }
+
+    void Update() {
+        // Vector3 dir = player.
+        transform.position = Vector3.Lerp(transform.position, player.position + offset, Time.deltaTime * lerpSpeed);
+        transform.position = transform.position.WithValues(z: 0);
+
+        if (GetInput.Forward()) {
+            offset.x = -1;
+        } else if (GetInput.Back()) {
+            offset.x = 1;
+        }
+    } 
+
+    [ContextMenu("Bake Offset")]
+    private void BakeOffset() {
+        offset = transform.position - player.transform.position;
+    }
     
     public void TakeDamage(float damage) {
         Instantiate(explosionParticle, transform.position, Quaternion.identity);
---
+++ Assets/_Scripts/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour, ITakeDamage {
+
+    public float spawnWait = 3f;
+
+    public GameObject prefab;
+    public Vector3 range = Vector3.zero;
+    
+    private void Start() {
+        StartCoroutine(Spawn());
+    }
+
+    //only relevant if spawner is destroyable
+    public void TakeDamage(float damage) {
+        
+    }
+
+    IEnumerator Spawn() {
+
+        while (true) {
+
+            Vector3 spawnPos = transform.position;
+            spawnPos += new Vector3(Random.Range(-range.x, range.x), Random.Range(-range.y, range.y),
+                Random.Range(-range.z, range.z));
+
+            Instantiate(prefab, spawnPos, Quaternion.identity);
+            
+            yield return new WaitForSeconds(spawnWait);
+
+        }
+        
+        
+    }
+    
+    
+}
