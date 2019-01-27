26650d65dd3f7660bb3dec939810d87f6ba9ff86
Ali Kedwaii <ali.kedwaii@utexas.edu>
2019-01-27 13:53:59 -0600

    hub music + adjust sfx vols

---
+++ Assets/_Scripts/IntroTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroTimer : MonoBehaviour
+{
+
+    private AudioSource mainSource;
+    private float timer;
+    private bool start;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainSource = GetComponent<AudioSource>();
+        timer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 22.588f && !start){
+            mainSource.Play();
+            start = true;
+        }
+    }
+}
