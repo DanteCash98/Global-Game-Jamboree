294062c613cb66091f79503d87d2e679e5bf104d
Dante Cash <DanteCash98@gmail.com>
2019-01-27 00:32:04 -0600

    Fireflies by Owl City

---
+++ Assets/_Scripts/PlayerLight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLight : MonoBehaviour
+{
+   public float PlayerHealth = 10f;
+   private float LampLight = 10f;
+
+
+   public void AddLight(float amount)
+   {
+      PlayerHealth += amount;
+   }
+   
+   
+}
